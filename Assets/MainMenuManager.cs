3701cb9e11ad132427151398c58d078a2b7755b1
TYbarra700 <thomas.j.ybarra@icloud.com>
2024-11-26 14:04:13 -0600

    angel done
    added finishing touches to angels, got hub menu working properly

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    // References to the menus
+    public GameObject mainMenu;
+    public GameObject controlsMenu;
+
     // This function loads the next scene based on the current scene index
     public void StartGame()
     {
@@ -16,5 +20,18 @@ public class MainMenuManager : MonoBehaviour
         Debug.Log("Exiting Game"); // This will only show in the Unity Editor
         Application.Quit();
     }
-}
 
+    // This function hides the main menu and shows the controls menu
+    public void ShowControlsMenu()
+    {
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+    }
+
+    // This function hides the controls menu and shows the main menu
+    public void BackToMainMenu()
+    {
+        controlsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
